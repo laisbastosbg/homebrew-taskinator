b6e7e0917f6eb444cece0bc96d75fa0b5139a33e
Lais Godinho <lais2bg@gmail.com>
2022-03-28 15:25:41 -0300

    ☹️

--- taskinator.rb
+++ taskinator.rb
@@ -1,7 +1,7 @@
 class Taskinator < Formula
   desc "Quickly manage your tasks from your terminal"
   homepage "https://github.com/laisbastosbg/taskinator"
-  url "https://github.com/laisbastosbg/taskinator/archive/refs/tags/0.0.2.tar.gz"
+  url "https://github.com/laisbastosbg/taskinator/releases/download/0.0.2/taskinator"
   sha256 "0bd7b7db62f90719b7f11e53a25749e0e1a1742c7818e113950b29c0ca326779"
   license "MIT"
 
