cd16fbeaf106a9d39b6a0bece79f6de2cf7179f8
Lais Godinho <lais2bg@gmail.com>
2022-03-28 15:40:05 -0300

    funciona 😡

--- taskinator.rb
+++ taskinator.rb
@@ -1,11 +1,1 @@
-class Taskinator < Formula
-  desc "Quickly manage your tasks from your terminal"
-  homepage "https://github.com/laisbastosbg/taskinator"
-  url "https://github.com/laisbastosbg/taskinator/releases/download/0.0.2/taskinator"
-  sha256 "a6dd23ca6f97f3c7e2cb9545ddd35b781fbd78b66ea0d5be1abbc41c085ce616"
-  license "MIT"
 
-  def install
-    bin.install "taskinator"
-  end
-end
